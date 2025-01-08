500ca97c4ad502d2ad38d11b1340a96293e55c13
Maria <marya.kotova@yandex.ru>
2025-01-08 14:27:19 +0300

    Iter 5.5

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"flag"
-	"os"
+	"log"
+
+	"github.com/caarlos0/env"
 )
 
 var netAddress string
 
+type Config struct {
+	ServerAddress string `env:"ADDRESS"`
+}
+
 func parseFlags() {
 
-	flag.StringVar(&netAddress, "a", "localhost:8080", "Адрес и порт для HTTP-сервера")
+	var cfg Config
+	err := env.Parse(&cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	flag.StringVar(&netAddress, "a", "localhost:8080", "Адрес эндпоинта HTTP-сервера")
 	flag.Parse()
 
-	if envServAddr := os.Getenv("ADDRESS"); envServAddr != "" {
-		netAddress = envServAddr
+	if cfg.ServerAddress != "" {
+		netAddress = cfg.ServerAddress
 	}
 }
+
+// func parseFlags() {
+
+// 	flag.StringVar(&netAddress, "a", "localhost:8080", "Адрес и порт для HTTP-сервера")
+// 	flag.Parse()
+
+// 	if envServAddr := os.Getenv("ADDRESS"); envServAddr != "" {
+// 		netAddress = envServAddr
+// 	}
+// }
