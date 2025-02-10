d20edb12772c6b064aa6796fe1467dc68118d45f
Maria <marya.kotova@yandex.ru>
2025-02-10 23:37:15 +0300

    Iter 13.4

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net"
 	"time"
 
 	"github.com/maryakotova/metrics/internal/collector"
@@ -18,27 +20,6 @@ func main() {
 
 	parseFlags()
 
-	n := int64(0)
-	collector.SetPollCountInitial()
-
-	for {
-		time.Sleep(time.Duration(pollInterval) * time.Second)
-		n += pollInterval
-
-		metrics := collector.CollectMetricsForBatch()
-
-		if len(metrics) > 0 {
-			if reportInterval == n {
-				n = 0
-				collector.SetPollCountInitial()
-
-				if err := sender.SendMetricsBatch(serverAddress, metrics); err != nil {
-					fmt.Printf("Ошибка при отправке метрик: %s\n", err)
-				}
-			}
-		}
-	}
-
 	// n := int64(0)
 	// collector.SetPollCountInitial()
 
@@ -46,48 +27,69 @@ func main() {
 	// 	time.Sleep(time.Duration(pollInterval) * time.Second)
 	// 	n += pollInterval
 
-	// 	metrics := collector.CollectMetrics()
+	// 	metrics := collector.CollectMetricsForBatch()
 
-	// 	if reportInterval == n {
-	// 		n = 0
-	// 		collector.SetPollCountInitial()
+	// 	if len(metrics) > 0 {
+	// 		if reportInterval == n {
+	// 			n = 0
+	// 			collector.SetPollCountInitial()
 
-	// 		retries := 0
-	// 		for retries < 4 {
-
-	// 			err := sender.SendMetrics(serverAddress, metrics)
-	// 			if err == nil {
-	// 				break
+	// 			if err := sender.SendMetricsBatch(serverAddress, metrics); err != nil {
+	// 				fmt.Printf("Ошибка при отправке метрик: %s\n", err)
 	// 			}
-
-	// 			var opErr *net.OpError
-	// 			if !errors.As(err, &opErr) {
-	// 				break
-	// 			}
-
-	// 			if retries == 3 {
-	// 				fmt.Println("ошибка соединения: %w", err)
-	// 				return
-	// 			}
-
-	// 			time.Sleep(time.Duration(retries*2+1) * time.Second) // Backoff: 1s, 3s, 5s
-	// 			retries++
-
 	// 		}
-
-	// 		// for metricName, metricValue := range metrics {
-	// 		// 	var metricType string
-	// 		// 	if metricName == constants.PollCount {
-	// 		// 		metricType = constants.Counter
-	// 		// 	} else {
-	// 		// 		metricType = constants.Gauge
-	// 		// 	}
-	// 		// 	err := sender.SendMetric(serverAddress, metricType, metricName, metricValue)
-
-	// 		// 	if err != nil {
-	// 		// 		fmt.Printf("Ошибка при отправке метрики %s: %s\n", metricName, err)
-	// 		// 	}
-	// 		// }
 	// 	}
 	// }
+
+	n := int64(0)
+	collector.SetPollCountInitial()
+
+	for {
+		time.Sleep(time.Duration(pollInterval) * time.Second)
+		n += pollInterval
+
+		metrics := collector.CollectMetrics()
+
+		if reportInterval == n {
+			n = 0
+			collector.SetPollCountInitial()
+
+			retries := 0
+			for retries < 4 {
+
+				err := sender.SendMetrics(serverAddress, metrics)
+				if err == nil {
+					break
+				}
+
+				var opErr *net.OpError
+				if !errors.As(err, &opErr) {
+					break
+				}
+
+				if retries == 3 {
+					fmt.Println("ошибка соединения: %w", err)
+					return
+				}
+
+				time.Sleep(time.Duration(retries*2+1) * time.Second) // Backoff: 1s, 3s, 5s
+				retries++
+
+			}
+
+			// for metricName, metricValue := range metrics {
+			// 	var metricType string
+			// 	if metricName == constants.PollCount {
+			// 		metricType = constants.Counter
+			// 	} else {
+			// 		metricType = constants.Gauge
+			// 	}
+			// 	err := sender.SendMetric(serverAddress, metricType, metricName, metricValue)
+
+			// 	if err != nil {
+			// 		fmt.Printf("Ошибка при отправке метрики %s: %s\n", metricName, err)
+			// 	}
+			// }
+		}
+	}
 }
