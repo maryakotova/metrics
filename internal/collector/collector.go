d000ff25c48ad745c2afe10c4131e9d868ba1b40
Maria <marya.kotova@yandex.ru>
2025-02-11 09:49:01 +0300

    Iter 13.7

--- internal/collector/collector.go
+++ internal/collector/collector.go
@@ -106,38 +106,38 @@ func CollectMetrics() map[string]interface{} {
 
 	metrics := make(map[string]interface{})
 
-	// metrics["Alloc"] = memStats.Alloc
-	// metrics["BuckHashSys"] = memStats.BuckHashSys
-	// metrics["Frees"] = memStats.Frees
-	// metrics["GCCPUFraction"] = memStats.GCCPUFraction
-	// metrics["GCSys"] = memStats.GCSys
-	// metrics["HeapAlloc"] = memStats.HeapAlloc
-	// metrics["HeapIdle"] = memStats.HeapIdle
-	// metrics["HeapInuse"] = memStats.HeapInuse
-	// metrics["HeapObjects"] = memStats.HeapObjects
-	// metrics["HeapReleased"] = memStats.HeapReleased
-	// metrics["HeapSys"] = memStats.HeapSys
-	// metrics["LastGC"] = memStats.LastGC
-	// metrics["Lookups"] = memStats.Lookups
-	// metrics["MCacheInuse"] = memStats.MCacheInuse
-	// metrics["MCacheSys"] = memStats.MCacheSys
-	// metrics["MSpanInuse"] = memStats.MSpanInuse
-	// metrics["MSpanSys"] = memStats.MSpanSys
-	// metrics["Mallocs"] = memStats.Mallocs
-	// metrics["NextGC"] = memStats.NextGC
-	// metrics["NumForcedGC"] = memStats.NumForcedGC
-	// metrics["NumGC"] = memStats.NumGC
-	// metrics["OtherSys"] = memStats.OtherSys
-	// metrics["PauseTotalNs"] = memStats.PauseTotalNs
-	// metrics["StackInuse"] = memStats.StackInuse
-	// metrics["StackSys"] = memStats.StackSys
-	// metrics["Sys"] = memStats.Sys
-	// metrics["TotalAlloc"] = memStats.TotalAlloc
+	metrics["Alloc"] = memStats.Alloc
+	metrics["BuckHashSys"] = memStats.BuckHashSys
+	metrics["Frees"] = memStats.Frees
+	metrics["GCCPUFraction"] = memStats.GCCPUFraction
+	metrics["GCSys"] = memStats.GCSys
+	metrics["HeapAlloc"] = memStats.HeapAlloc
+	metrics["HeapIdle"] = memStats.HeapIdle
+	metrics["HeapInuse"] = memStats.HeapInuse
+	metrics["HeapObjects"] = memStats.HeapObjects
+	metrics["HeapReleased"] = memStats.HeapReleased
+	metrics["HeapSys"] = memStats.HeapSys
+	metrics["LastGC"] = memStats.LastGC
+	metrics["Lookups"] = memStats.Lookups
+	metrics["MCacheInuse"] = memStats.MCacheInuse
+	metrics["MCacheSys"] = memStats.MCacheSys
+	metrics["MSpanInuse"] = memStats.MSpanInuse
+	metrics["MSpanSys"] = memStats.MSpanSys
+	metrics["Mallocs"] = memStats.Mallocs
+	metrics["NextGC"] = memStats.NextGC
+	metrics["NumForcedGC"] = memStats.NumForcedGC
+	metrics["NumGC"] = memStats.NumGC
+	metrics["OtherSys"] = memStats.OtherSys
+	metrics["PauseTotalNs"] = memStats.PauseTotalNs
+	metrics["StackInuse"] = memStats.StackInuse
+	metrics["StackSys"] = memStats.StackSys
+	metrics["Sys"] = memStats.Sys
+	metrics["TotalAlloc"] = memStats.TotalAlloc
 
 	pollCount++
 	metrics["PollCount"] = pollCount
 
-	// metrics["RandomValue"] = rand.Float64()
+	metrics["RandomValue"] = rand.Float64()
 
 	return metrics
 }
