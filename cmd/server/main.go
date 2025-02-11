fcf3ba361250ad68b7bba225c3374f077a762dc5
Maria <marya.kotova@yandex.ru>
2025-02-11 14:49:19 +0300

    Iter 13.12

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -70,7 +70,7 @@ func main() {
 			worker.TriggerGoFunc(ticker, task)
 		}
 
-		server = handlers.NewServer(memStorage, syncFileWrite, writer)
+		server = handlers.NewServer(memStorage, syncFileWrite, writer, log)
 	}
 
 	router := chi.NewRouter()
