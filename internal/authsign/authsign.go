21333fbf74006988e5bbdc8a9d494a319243a004
Maria <marya.kotova@yandex.ru>
2025-02-22 15:10:34 +0300

    Iter 14.2

--- internal/authsign/authsign.go
+++ internal/authsign/authsign.go
@@ -3,6 +3,7 @@ package authsign
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/hex"
 )
 
 func VerifySig(receivedHash string, data []byte, key []byte) bool {
@@ -14,5 +15,5 @@ func CalculateHash(data []byte, key []byte) string {
 	hash := hmac.New(sha256.New, key)
 	hash.Write([]byte(data))
 	calculatedHash := hash.Sum(nil)
-	return string(calculatedHash)
+	return hex.EncodeToString(calculatedHash)
 }
