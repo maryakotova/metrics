c1defb95aa3a8dd4a4927facb15f0a9688a1982f
Maria <marya.kotova@yandex.ru>
2025-04-21 19:44:46 +0300

    test Iter 15.20

--- internal/agent/workerpool.go
+++ internal/agent/workerpool.go
@@ -30,28 +30,37 @@ func (agent *Agent) CollectAdditionalMetricsAtInterval() {
 func (agent *Agent) PublishMetrics() {
 	for range agent.reportTicker.C {
 		agent.mutex.Lock()
-		defer agent.mutex.Unlock()
 		agent.sendQueue <- Metrics{Metrics: agent.metrics}
 		// slog.Info(fmt.Sprintf("metrics published: %v", agent.metrics))
 		slog.Info("metrics published")
 		agent.ResetMetricsStorage()
 		agent.setPollCountInitial()
-
+		agent.mutex.Unlock()
 	}
 
 }
 
 func (agent *Agent) Worker(w int) {
-	// for metrics := range agent.sendQueue {
-	slog.Info(fmt.Sprintf("worker %v started", w))
-	defer agent.WG.Done()
-	for {
-		metrics := <-agent.sendQueue
-		// slog.Info(fmt.Sprintf("Worker %v: metrics read (%v)", w, metrics))
-		slog.Info(fmt.Sprintf("Worker %v: metrics read", w))
-		metricsForSend := agent.PrepareMetrics(metrics.Metrics)
-		err := agent.SendMetricsBatch(metricsForSend)
-		agent.resultQueue <- Result{Metrics: metrics.Metrics, Error: err}
+	for range agent.reportTicker.C {
+		//   for metrics := range agent.sendQueue {
+		//     metricsForSend := agent.PrepareMetrics(metrics.Metrics)
+		//     err := agent.SendMetricsBatch(metricsForSend)
+		//     if err != nil {
+		//       agent.resultQueue <- Result{Metrics: metrics.Metrics, Error: err}
+		//     }
+		//   }
+		// }
+		for {
+			metrics, ok := <-agent.sendQueue
+			if !ok {
+				break
+			}
+			metricsForSend := agent.PrepareMetrics(metrics.Metrics)
+			err := agent.SendMetricsBatch(metricsForSend)
+			if err != nil {
+				agent.resultQueue <- Result{Metrics: metrics.Metrics, Error: err}
+			}
+		}
 	}
 }
 
